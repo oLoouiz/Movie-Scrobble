100d8a6b95d2b036d71aa19de12ac36629478c57
oLoouiz <loouiz.felipe@hotmail.com>
2025-02-18 21:52:22 -0300

    ops

--- IMDB_API.py
+++ IMDB_API.py
@@ -5,7 +5,7 @@ conn = http.client.HTTPSConnection("netflix-movies-and-tv-shows1.p.rapidapi.com"
 payload = "page=1&type=Movie&release_year=1975"
 
 headers = {
-    'x-rapidapi-key': "3026fd4d1emsh64dccbf280ca76ep199392jsn87474bc53edc",
+    'x-rapidapi-key': "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxc",
     'x-rapidapi-host': "netflix-movies-and-tv-shows1.p.rapidapi.com",
     'Content-Type': "application/x-www-form-urlencoded"
 }
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Movie-Scrobble
-Projeto dos crias
